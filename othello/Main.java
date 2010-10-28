31e5b49cf3703210e7e3a3daa8b0da4f1fadff70
Periklis Ntanasis <pntanasis@gmail.com>
2010-10-28 03:54:43 +0300

    Main class

---
+++ othello/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Creative Commons Attribution Non-Commercial Share Alike
+ */
+
+package othello;
+
+/**
+ *
+ * @author Periklis Ntanasis
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
