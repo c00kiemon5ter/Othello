08166ae0c02b9f5c6c036dcb12969b014fb66e40
Ivan Kanakarakis <ivan.kanak@gmail.com>
2010-11-26 04:32:49 +0200

    player enum

--- src/core/Player.java
+++ src/core/Player.java
@@ -3,19 +3,39 @@
  */
 package core;
 
-public class Player {
+public enum Player {
 
-	private String nickname;
-	private final boolean color;
+	BLACK(DiskState.BLACK),
+	WHITE(DiskState.WHITE);
+	private String description;
+	private DiskState color;
 	private int score;
 
-	public Player(String nickname, boolean color) {
-		this.nickname = nickname;
+	private Player(DiskState color) {
+		this.description = "The " + (color == DiskState.BLACK
+					     ? "black" : "white") + " player";
 		this.color = color;
+		this.score = 2;
 	}
 
-	public void turn() {
-		// discover available moves
+	public DiskState color() {
+		return color;
 	}
 
+	public String description() {
+		return description;
+	}
+
+	public int score() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s: %d", this.description, this.score);
+	}
 }
