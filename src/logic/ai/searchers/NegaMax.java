74e11c0c7cf3cdde1cacf57bcd0651f0b17504cd
Ivan Kanakarakis <ivan.kanak@gmail.com>
2010-11-30 13:20:42 +0200

    negamax with ab-pruning

--- src/logic/ai/searchers/NegaMax.java
+++ src/logic/ai/searchers/NegaMax.java
@@ -7,34 +7,70 @@ import java.util.Set;
 import java.awt.Point;
 import logic.ai.evaluation.Evaluation;
 
-public class NegaMax extends AbstractSearcher {
+public class NegaMax extends AbstractSearcher implements Searcher, SimpleSearcher {
 
-	/* search( board , player , Integer.MIN_VALUE , Integer.MAX_VALUE , depth , ef ) */
 	@Override
 	public int search(Board board, Player player, int alpha, int beta, int depth, Evaluation function) {
-		int record = Integer.MIN_VALUE;
-		if (depth <= 0 || super.isEndState(board)) {
-			record = function.evaluate(board, player);
+		Point maxMove = null;
+		if (depth <= 0 || isEndState(board)) {
+			alpha = function.evaluate(board, player);
 		} else {
+			Board subBoard = board.clone();
 			Set<Point> possibleMoves = MoveExplorer.explore(board, player.color());
 			if (!possibleMoves.isEmpty()) {
 				for (Point nextPossibleMove : possibleMoves) {
-					Board subBoard = board.clone();
+					subBoard = board.clone();
 					subBoard.makeMove(nextPossibleMove, player.color());
-					record = max(Integer.MIN_VALUE, -search(subBoard, player.opponent(), alpha, beta, depth - 1, function));
+					int value = -search(subBoard, player.opponent(), depth - 1, -beta, -alpha, function);
+//					record = max(record, -simpleSearch(subBoard, player.opponent(), depth - 1, function));
+					if (value >= beta) {
+						maxMove = nextPossibleMove;
+						return beta;
+					}
+					if (value > alpha) {
+						maxMove = nextPossibleMove;
+						alpha = value;
+					}
 				}
-//			} else {
-//				possibleMoves = MoveExplorer.explore(board, player.color().opposite());
-//				if (possibleMoves.isEmpty()) {
-//				} else {
-//					record = max(Integer.MIN_VALUE, search(board, player, depth - 2, -ev_sign, function));
-//				}
+			} else {
+				int value = -simpleSearch(subBoard, player, depth - 1, function);
+				if (value >= beta) {
+					return beta;
+				}
+				if (value > alpha) {
+					alpha = value;
+				}
 			}
 		}
-		return record;
+		bestMove = maxMove;
+		return alpha;
 	}
 
-	private int max(int a, int b) {
-		return a < b ? b : a;
+	@Override
+	public int simpleSearch(Board board, Player player, int depth, Evaluation function) {
+		int record = Integer.MIN_VALUE;
+		Point maxMove = null;
+		if (depth <= 0 || isEndState(board)) {
+			record = function.evaluate(board, player);
+		} else {
+			Board subBoard = board.clone();
+			Set<Point> possibleMoves = MoveExplorer.explore(board, player.color());
+			if (!possibleMoves.isEmpty()) {
+				for (Point nextPossibleMove : possibleMoves) {
+					subBoard = board.clone();
+					subBoard.makeMove(nextPossibleMove, player.color());
+					int result = -simpleSearch(subBoard, player.opponent(), depth - 1, function);
+//					record = max(record, -simpleSearch(subBoard, player.opponent(), depth - 1, function));
+					if (result > record) {
+						record = result;
+						maxMove = nextPossibleMove;
+					}
+				}
+			} else {
+				record = -simpleSearch(subBoard, player, depth - 1, function);
+			}
+		}
+		bestMove = maxMove;
+		return record;
 	}
 }
