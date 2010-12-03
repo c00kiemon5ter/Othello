bc19193c38fda3dfe86c54371e81b0c75ebfecc6
Ivan Kanakarakis <ivan.kanak@gmail.com>
2010-12-03 16:22:19 +0200

    negamax documentation

--- src/logic/ai/searchers/NegaMax.java
+++ src/logic/ai/searchers/NegaMax.java
@@ -7,11 +7,27 @@ import java.util.Set;
 import java.awt.Point;
 import logic.ai.evaluation.Evaluation;
 
+/**
+ * <p>Negamax search is a slightly variant formulation of minimax search
+ * that relies on the zero-sum property of a two-player game.</p>
+ * <p>By definition the value of a position to player A in such a game is
+ * the negation of the value to player B. Thus, the player on move looks
+ * for a move that maximizes the negation of the value of the position
+ * resulting from the move: this successor position must by definition
+ * have been valued by the opponent. </p>
+ * <p>The reasoning of the previous sentence works regardless of whether
+ * A or B is on move. This means that a single computation can be used to
+ * value all positions</p>
+ * <p>This is a coding simplification over minimax, which requires that A
+ * select the move with the maximum-valued successor while B selects the
+ * move with the minimum-valued successor.</p>
+ * 
+ * @author c00kiemon5ter
+ */
 public class NegaMax extends AbstractSearcher implements Searcher, SimpleSearcher {
 
 	@Override
-
-        public int search(Board board, Player player, int alpha, int beta, int depth, Evaluation function) {
+	public int search(Board board, Player player, int alpha, int beta, int depth, Evaluation function) {
 		int record = alpha;
 		Point maxMove = null;
 		if (depth <= 0 || isEndState(board)) {
@@ -23,12 +39,13 @@ public class NegaMax extends AbstractSearcher implements Searcher, SimpleSearche
 				for (Point nextPossibleMove : possibleMoves) {
 					subBoard = board.clone();
 					subBoard.makeMove(nextPossibleMove, player.color());
-					int result = -search(subBoard, player.opponent(),-beta,-alpha, depth - 1, function);
+					int result = -search(subBoard, player.opponent(), -beta, -alpha, depth - 1, function);
 					if (result > record) {
 						record = result;
 						maxMove = nextPossibleMove;
-                                                if(record >= beta)
-                                                    return record;
+						if (record >= beta) {
+							return record;
+						}
 					}
 				}
 			} else {
