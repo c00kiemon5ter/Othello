b3be795b7f04b302c855a5c5a84f641d4660b876
Ivan Kanakarakis <ivan.kanak@gmail.com>
2010-12-03 15:26:51 +0200

    weighted evaluator considering corner placement

---
+++ src/logic/ai/evaluation/ScoreCornerWeightEval.java
@@ -0,0 +1,37 @@
+package logic.ai.evaluation;
+
+import core.Board;
+import core.Player;
+import java.awt.Point;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ScoreCornerWeightEval implements Evaluation {
+
+	private Set<Point> corners;
+	private int weight;
+
+	public ScoreCornerWeightEval(int weight) {
+		this.weight = weight;
+		this.corners = new HashSet<Point>(4);
+		this.corners.add(new Point(0, 0));
+		this.corners.add(new Point(Board.BOARD_LENGTH, 0));
+		this.corners.add(new Point(0, Board.BOARD_WIDTH));
+		this.corners.add(new Point(Board.BOARD_LENGTH, Board.BOARD_WIDTH));
+	}
+
+	public ScoreCornerWeightEval() {
+		this(1);
+	}
+
+	@Override
+	public int evaluate(Board board, Player player) {
+		int score = board.count(player.color());
+		for (Point p : corners) {
+			if (board.getSquareState(p) == player.color()) {
+				score += weight;
+			}
+		}
+		return score;
+	}
+}
